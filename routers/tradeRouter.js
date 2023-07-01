153320a5748cf263e2b0d91680654d3161e17877
JasonGal <jason.gal.bu@gmail.com>
2023-07-01 19:11:00 +0300

    trades router

---
+++ routers/tradeRouter.js
@@ -0,0 +1,10 @@
+const router = require("express").Router();
+const tradeController = require("../controllers/tradeController.js");
+
+router.get("/", tradeController.getAllTrades);
+router.post("/", tradeController.postOneTrade);
+router.delete("/:id", tradeController.deleteTrade);
+router.put("/:id", tradeController.updateTrade);
+router.get("/:userId", tradeController.getAllUserTrades);
+
+module.exports = router;
