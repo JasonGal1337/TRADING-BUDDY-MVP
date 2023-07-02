8dea7d3d80f346592ed54dc5edfa156965d6ffaf
JasonGal <jason.gal.bu@gmail.com>
2023-07-02 19:26:33 +0300

    fixed routing errors, backend ready, trade schema will be updated after frontend

--- controllers/tradeController.js
+++ controllers/tradeController.js
@@ -1,33 +1,33 @@
 const Trade = require('../modules/trade.js');
 
 const getAllTrades = async (req,res) => {
-    const Trade = await Trade.find();
-    res.send(Trade);
+    const trades = await Trade.find();
+    res.send(trades);
 };
 
 const getOneTrade = async (req,res) => {
-    const Trade = await Trade.findOne({ _id: req.params.id });
-    res.send(Trade);
+    const trade = await Trade.findOne({ _id: req.params.id });
+    res.send(trade);
 };
 
 const postOneTrade = async (req,res) => {
-    const Trade = await Trade.create(req.body);
+    const newTrade = await Trade.create(req.body);
     res.send({ msg: "trade logged successfully" });
 };
 
 const deleteTrade = async (req,res) => {
-    const Trade = await Trade.deleteOne({ _id: req.params.id });
+    const deletedTrade = await Trade.deleteOne({ _id: req.params.id });
     res.send({ msg: "trade deleted" });
 };
 
 const updateTrade = async (req,res) => {
-    const Trade = await Trade.findByIdAndUpdate({ _id: req.params.id }, req.body);
+    const updatedTrade = await Trade.findByIdAndUpdate({ _id: req.params.id }, req.body);
     res.send({ msg: "trade updated "});
 };
 
 const getAllUserTrades = async (req,res) => {
-    const Trade = await Trade.find({ userId: req.params.userId });
-    res.send(Trade);
+    const userTrades = await Trade.find({ userId: req.params.userId });
+    res.send(userTrades);
 };
 
 module.exports = {
