5305f89d9167b8a5806bf4ea49f4edb60004dba4
JasonGal <jason.gal.bu@gmail.com>
2023-07-07 21:40:39 +0300

    fixed issue where trades were not displaying

--- client/src/profile.js
+++ client/src/profile.js
@@ -1,6 +1,5 @@
 import { useNavigate } from 'react-router-dom';
 import "./index.css";
-import TradeForm from "./tradeForm.js";
 import { useState, useEffect } from "react";
 import axios from "axios";
 
@@ -9,6 +8,14 @@ function Profile() {
   const [user, setUser] = useState({
     _id: "",
     email: "",
+  });
+  const [trade, setTrade] = useState({
+    pair: "",
+    buyingPrice: null,
+    sellingPrice: null,
+    amount: null,
+    date: "",
+    userId: ""
   });
   const [trades, setTrades] = useState([]);
 
@@ -19,28 +26,114 @@ function Profile() {
           token: localStorage.getItem("token"),
         })
         .then(({ data }) => {
-          if (data._id) {
-            console.log(data);
-            setUser(data.data);
+          if (data.userData._id) {
+            console.log(data.userData);
+            setUser(data.userData);
             axios
-              .get("http://localhost:3001/trade/" + data._id)
+              .get("http://localhost:3001/trade/" + data.userData._id)
               .then(({ data }) => {
                 console.log("user trades", data);
                 setTrades(data);
               });
           }
         });
+    } else {
+      navigate("/");
     }
   }, []);
+
+  const handleTradeChange = (event) => {
+    const { name, value } = event.target;
+    setTrade((prevTrade) => ({
+      ...prevTrade,
+      [name]: value,
+      userId: user._id
+    }));
+  };
+
+  const handleTradeSubmit = (event) => {
+    event.preventDefault();
+    console.log(trade);
+
+    axios
+      .post("http://localhost:3001/trade", trade)
+      .then((res) => {
+        console.log(res.data);
+        setTrade({
+          pair: "",
+          buyingPrice: null,
+          sellingPrice: null,
+          amount: null,
+          date: "",
+          userId: ""
+        });
+        setTrades((prevTrades) => [...prevTrades, res.data]); // Add the new trade to trades state
+      })
+      .catch((error) => {
+        console.error("Error sending trade data:", error);
+      });
+  };
 
   return (
     <div>
       <h1>Hello user {user.email}!</h1>
       <h2>Log a Trade:</h2>
-      <TradeForm />
+      <form onSubmit={handleTradeSubmit}>
+        <label>
+          Pair:
+          <input
+            type="text"
+            name="pair"
+            value={trade.pair}
+            onChange={handleTradeChange}
+          />
+        </label>
+        <br />
+        <label>
+          Buying Price:
+          <input
+            type="number"
+            name="buyingPrice"
+            value={trade.buyingPrice || ""}
+            onChange={handleTradeChange}
+          />
+        </label>
+        <br />
+        <label>
+          Selling Price:
+          <input
+            type="number"
+            name="sellingPrice"
+            value={trade.sellingPrice || ""}
+            onChange={handleTradeChange}
+          />
+        </label>
+        <br />
+        <label>
+          Amount:
+          <input
+            type="number"
+            name="amount"
+            value={trade.amount || ""}
+            onChange={handleTradeChange}
+          />
+        </label>
+        <br />
+        <label>
+          Date:
+          <input
+            type="text"
+            name="date"
+            value={trade.date}
+            onChange={handleTradeChange}
+          />
+        </label>
+        <br />
+        <button type="submit">Submit</button>
+      </form>
       <div className="trading-log">
         {trades.map((trade) => (
-          <div key={trade.id} className="trade-item">
+          <div key={trade._id} className="trade-item">
             <p>Pair: {trade.pair}</p>
             <p>Buying Price: {trade.buyingPrice}</p>
             <p>Selling Price: {trade.sellingPrice}</p>
